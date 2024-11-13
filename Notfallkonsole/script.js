1ab1ca137897b928592c9035c89de65f006dd358
Levin Schmid <levin.schmid@mein.gmx>
2024-11-13 11:11:53 +0100

    finisch notfallkonsole

---
+++ Notfallkonsole/script.js
@@ -0,0 +1,17 @@
+function hoverAn(e) {
+    e.target.parentElement.style.color = "red";
+    e.target.parentElement.classList.add("hover");
+}
+function hoverAb (e) { 
+    e.target.parentElement.style.color = "white";
+    e.target.parentElement.classList.remove("hover");
+}
+function initialize() {
+    var schalter = ["antrieb", "holodeck", "hitzeschild", "transporter"];
+    for (var i = 0; i < schalter.length; i++) {
+        document.getElementById("schalter-" + schalter[i]).addEventListener("mouseover", hoverAn);
+        document.getElementById("schalter-" + schalter[i]).addEventListener("mouseout", hoverAb);
+    }   
+}
+window.addEventListener("load", initialize);
+
