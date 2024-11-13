e0a4752689047b1ab14f7f0a157b6a013e4f1d77
Levin Schmid <levin.schmid@mein.gmx>
2024-11-13 11:31:47 +0100

    start DPM-parser

---
+++ dom-parser/parser.js
@@ -0,0 +1,9 @@
+var result = "";
+
+function parseNode(startNode) {
+    //todo
+    //für jeden Kindknoten ab "startNode" eine Tabellenzeile (<tr><td>....</tr>) 
+    //mit den gemäss Aufgabenstellung gewünschten Infos erstellen
+
+    return "<table><tr><th>nodeType</th><th>nodeName</th><th>nodeValue</th><th>textContent</th></tr>" + result + "</table>";
+}
