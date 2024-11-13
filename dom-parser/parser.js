57fb17320bef64086c4e3898fa259fd63406f9bb
Levin Schmid <levin.schmid@mein.gmx>
2024-11-13 14:41:08 +0100

    ,.

--- dom-parser/parser.js
+++ dom-parser/parser.js
@@ -1,9 +1,31 @@
-var result = "";
 
-function parseNode(startNode) {
-    //todo
-    //für jeden Kindknoten ab "startNode" eine Tabellenzeile (<tr><td>....</tr>) 
-    //mit den gemäss Aufgabenstellung gewünschten Infos erstellen
+function parseNode(aktuellTag) {
 
+    var result = "";
+    var element = aktuellTag.childNodes;
+
+    for  (var child in element){
+        console.log(child)
+        travelThroughNode(aktuellTag)
+    }
+        
+    
+    
     return "<table><tr><th>nodeType</th><th>nodeName</th><th>nodeValue</th><th>textContent</th></tr>" + result + "</table>";
+
+    function travelThroughNode(aktuellTag){
+            result += "<tr>" +
+            "<td>" + aktuellTag.nodeType + "</td>" + 
+            "<td>" + aktuellTag.nodeName + "</td>" + 
+            "<td>" + aktuellTag.nodeValue + "</td>" +
+            "<td>" + aktuellTag.textContent + "</td>" +
+            "</tr>" ;
+            if (aktuellTag.childNode){
+                travelThroughNode(aktuellTag[i])
+            }
+    }
+    
+    
+    
 }
+
