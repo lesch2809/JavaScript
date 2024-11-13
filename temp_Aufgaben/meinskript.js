1fd88424e8be793b66d01ad1c4e44ec8bdf1cbf1
Levin Schmid <levin.schmid@mein.gmx>
2024-11-13 09:52:53 +0100

    .

--- temp_Aufgaben/meinskript.js
+++ temp_Aufgaben/meinskript.js
@@ -1,11 +1,7 @@
+var a = 2;
+document.write(quadrieren(a)); 
+document.write(a);
 
-    var n1 = "3a";
-    var n2 = "3";
-    if (n1 != 3) {
-        n1 = Number.NaN;
-    }
-    if (n2 != 3) {
-        n2 = Number.NaN;
-    }
-    alert(n1);   //?
-    alert(n2);   //?
+function quadrieren(zahl) {
+    zahl *= zahl;
+}
