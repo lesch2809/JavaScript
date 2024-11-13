6956e47b352c2572c3781949086451b488bcc6cd
Levin Schmid <levin.schmid@mein.gmx>
2024-11-13 09:22:00 +0100

    .

--- temp_Aufgaben/meinskript.js
+++ temp_Aufgaben/meinskript.js
@@ -1,1 +1,11 @@
-document.write("Hallo Welt"); 
+
+    var n1 = "3a";
+    var n2 = "3";
+    if (n1 != 3) {
+        n1 = Number.NaN;
+    }
+    if (n2 != 3) {
+        n2 = Number.NaN;
+    }
+    alert(n1);   //?
+    alert(n2);   //?
