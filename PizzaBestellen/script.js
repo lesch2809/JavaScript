bc1519e44109d73c85c8a81c5a46b02c867c7b5b
Levin Schmid <levin.schmid@mein.gmx>
2024-11-14 09:12:03 +0100

    finisch pizza

--- PizzaBestellen/script.js
+++ PizzaBestellen/script.js
@@ -9,18 +9,28 @@ function add() {
     var ingredients = document.getElementById("ingredients");
     
     if (ingredients.selectedIndex > -1 ) {
-        var idx = ingredients.selectedIndex
-        var ingredient = ingredients.getElementsByTagName("option")[idx]
-        ingredient.remove()
+        var idxI = ingredients.selectedIndex
+        if (ingredients.getElementsByTagName("option")[idxI].textContent == "Ananas")  {
+            document.getElementsByTagName("body")[0].setAttribute("style", "background-color:red;")
+            
+        }else{
+        
+        var ingredientI = ingredients.getElementsByTagName("option")[idxI]
+        choice.appendChild(ingredientI)
+        }
         
         //todo: ausgewählte Zutat von der linken in die rechte Listbox verschieben 
     }
+    
 }
 
 function remove() {
     var choice = document.getElementById("choice");
     
     if (choice.selectedIndex > -1 ) {
+        var idxC = choice.selectedIndex
+        var ingredientC = choice.getElementsByTagName("option")[idxC]
+        ingredients.appendChild(ingredientC)
         //todo: ausgewählte Zutat von der rechten in die linke Listbox verschieben
     }
 }
