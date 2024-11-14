7e9015091b415322f66614040bcccfc73571b0b2
Levin Schmid <levin.schmid@mein.gmx>
2024-11-14 13:57:29 +0100

    .

---
+++ tic-tac-toe/script.js
@@ -0,0 +1,13 @@
+document.getElementsByClassName("button1").addEventListener("click", place);
+        document.getElementsByClassName("button2").addEventListener("click", place);
+        document.getElementsByClassName("button3").addEventListener("click", place);
+        document.getElementsByClassName("button4").addEventListener("click", place);
+        document.getElementsByClassName("button5").addEventListener("click", place);
+        document.getElementsByClassName("button6").addEventListener("click", place);
+        document.getElementsByClassName("button7").addEventListener("click", place);
+        document.getElementsByClassName("button8").addEventListener("click", place);
+        document.getElementsByClassName("button9").addEventListener("click", place);
+        
+        function place(){
+
+        }
