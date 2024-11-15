c3eea77539c408cdfb0bb58e3f24f723851d816c
Levin Schmid <levin.schmid@mein.gmx>
2024-11-15 14:12:30 +0100

    .

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,10 +1,11 @@
 var buttons = document.getElementsByTagName("button");
-var player = 1
-var i
+var player = 1;
+var i;
 var Xwon = false;
 var Owon = false;
-var currentGameFieldO = []
-var currentGameFieldX = []
+var currentGameFieldO = [];
+var currentGameFieldX = [];
+var draw = [];
 var winningCombination = [
     [1, 2, 3],
     [4, 5, 6],
@@ -19,66 +20,70 @@ var winningCombination = [
 function place(eventObj) {
     var button = eventObj.target;
 
-    if (player === 1) {
+    if (player === 1 && button.innerHTML == "|") {
         button.innerHTML = "O";
         player = 0;
-        
-        
-        
-    } else {
+
+    } else if (player === 0 && button.innerHTML == "|") {
+
         button.innerHTML = "X";
         player = 1;
-        
-        
-        
+
+
     }
     checkifwon()
 }
 
 function checkifwon() {
-    currentGameFieldX = []
-    currentGameFieldO = []
-    for (var j = 1; j < 9; j++) {
-        var buttonNR=document.getElementById("button" + j).textContent
-        if (buttonNR==="X"){
-         currentGameFieldX.push(j)
+    currentGameFieldX = [];
+    currentGameFieldO = [];
+    draw = [];
+    for (var j = 1; j < 10; j++) {
+        var buttonNR = document.getElementById("button" + j).textContent
+        if (buttonNR === "X") {
+            currentGameFieldX.push(j);
+            draw.push(j);
         }
-        if (buttonNR==="O"){
-         currentGameFieldO.push(j)
+        if (buttonNR === "O") {
+            currentGameFieldO.push(j);
+            draw.push(j);
         }
-        
-    } alert(currentGameFieldO)
+
+
+    } 
 
     for (var i = 0; i < winningCombination.length; i++) {
-        if ( currentGameFieldX.includes(winningCombination[i][0])
+        if (currentGameFieldX.includes(winningCombination[i][0])
             && currentGameFieldX.includes(winningCombination[i][1])
             && currentGameFieldX.includes(winningCombination[i][2])) {
             Xwon = true;
         }
     }
     for (var i = 0; i < winningCombination.length; i++) {
-        if (currentGameFieldO.includes(winningCombination[i][0]) 
+        if (currentGameFieldO.includes(winningCombination[i][0])
             && currentGameFieldO.includes(winningCombination[i][1])
             && currentGameFieldO.includes(winningCombination[i][2])) {
             Owon = true;
         }
     }
     
-    if (Xwon === true){
-        alert("Xwon");
-    }
-    if (Owon === true){
-        alert("Owon");
-    }
-    
-}
 
+    if (Xwon === true) {
+        document.write("X has won");
+    }
+    else if (Owon === true) {
+        document.write(<p>O has won</p>);
+    }
+    else if (draw.length === 9) {
+        alert("Draw");
+    }
+}
 
 for (i = 0; i < 9; i++) {
     console.log(i);
     buttons[i].addEventListener("click", place);
 
-    
+
 }
 
 
