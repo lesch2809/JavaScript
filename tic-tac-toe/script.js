975be03b35c67c906c27a5b4d7983615d2a01778
Levin Schmid <levin.schmid@mein.gmx>
2024-11-15 08:26:06 +0100

    .

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,13 +1,29 @@
-document.getElementsByClassName("button1").addEventListener("click", place);
-        document.getElementsByClassName("button2").addEventListener("click", place);
-        document.getElementsByClassName("button3").addEventListener("click", place);
-        document.getElementsByClassName("button4").addEventListener("click", place);
-        document.getElementsByClassName("button5").addEventListener("click", place);
-        document.getElementsByClassName("button6").addEventListener("click", place);
-        document.getElementsByClassName("button7").addEventListener("click", place);
-        document.getElementsByClassName("button8").addEventListener("click", place);
-        document.getElementsByClassName("button9").addEventListener("click", place);
-        
-        function place(){
+var buttons = document.getElementsByTagName("button");
 
-        }
+function place(eventObj) {
+    var button = eventObj.target; 
+    button.innerHTML = "B";
+}
+
+
+for (var i = 0; i < 9; i++) {
+    console.log(i);
+    buttons[i].addEventListener("click", place);
+    /*() => {
+        button[i].innerHTML = "B";
+
+    }
+    );
+    */
+}
+
+/*
+button[1].addEventListener("click", place);
+button[2].addEventListener("click", place);
+button[3].addEventListener("click", place);
+button[4].addEventListener("click", place);
+button[5].addEventListener("click", place);
+button[6].addEventListener("click", place);
+button[7].addEventListener("click", place);
+button[8].addEventListener("click", place);
+*/
