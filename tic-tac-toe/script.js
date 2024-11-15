bc33dc179b0afdd2344295810c436551f72b75a6
Levin Schmid <levin.schmid@mein.gmx>
2024-11-15 11:29:01 +0100

    .

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,14 +1,75 @@
 var buttons = document.getElementsByTagName("button");
+var player = 1
+var i
+var currentGameFieldO = []
+var currentGameFieldX = []
+var winningCombination = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
+];
 
 function place(eventObj) {
-    var button = eventObj.target; 
-    button.innerHTML = "B";
+    var button = eventObj.target;
+
+    if (player === 1) {
+        button.innerHTML = "O";
+        player = 0;
+        
+        
+        
+    } else {
+        button.innerHTML = "X";
+        player = 1;
+        
+        
+        
+    }
+    checkifwon()
+}
+
+function checkifwon() {
+    currentGameFieldX = []
+    currentGameFieldO = []
+    for (var j = 1; j < 9; j++) {
+        var buttonNR=document.getElementById("button" + j).textContent
+        if (buttonNR==="X"){
+         currentGameFieldX.push(j)
+        }
+        if (buttonNR==="O"){
+         currentGameFieldO.push(j)
+        }
+        
+    } alert(currentGameFieldO)
+
+    for (var i = 0; i < winningCombination.length; i++) {
+        if (buttons[winningCombination[i][0]] == winningCombination[i][1] in currentGameFieldX && winningCombination[i][2] in currentGameFieldX) {
+            return true
+        }else{
+            return false
+        }
+    }
+    for (var i = 0; i < winningCombination.length; i++) {
+        if (winningCombination[i][0] in currentGameFieldO && winningCombination[i][1] in currentGameFieldO && winningCombination[i][2] in currentGameFieldO) {
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    alert(currentGameFieldO)
 }
 
 
-for (var i = 0; i < 9; i++) {
+for (i = 0; i < 9; i++) {
     console.log(i);
     buttons[i].addEventListener("click", place);
+
     /*() => {
         button[i].innerHTML = "B";
 
@@ -17,13 +78,4 @@ for (var i = 0; i < 9; i++) {
     */
 }
 
-/*
-button[1].addEventListener("click", place);
-button[2].addEventListener("click", place);
-button[3].addEventListener("click", place);
-button[4].addEventListener("click", place);
-button[5].addEventListener("click", place);
-button[6].addEventListener("click", place);
-button[7].addEventListener("click", place);
-button[8].addEventListener("click", place);
-*/
+
